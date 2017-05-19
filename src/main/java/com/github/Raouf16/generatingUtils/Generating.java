1590e828d36b733280afc5aa2563451812144fd4
serska15 <serska15@b042-05.crio.dauphine.fr>
2017-05-19 14:52:01 +0200

    dataframe

--- src/main/java/com/github/Raouf16/generatingUtils/Generating.java
+++ src/main/java/com/github/Raouf16/generatingUtils/Generating.java
@@ -13,25 +13,22 @@ import org.apache.spark.sql.catalog.Column;
  * Classe ayant pour but de générer les fichiers agrégés
  *
  */
-public class Generating 
-{
-	public static void generateAgregatedFile()
-	{
-		//TODO implement
-	}
-
-	public static void main (String []args)
-	{
+public class Generating {
+	public static void generateAgregatedFile(){
 		SparkSession spark = SparkSession
 				  .builder()
 				  .appName("Java Spark SQL basic example")
 				  .config("spark.master", "local")
 				  .getOrCreate();
-
 		Dataset<Row> df = spark.read().option("header", true).csv("src/main/resources/com/github/Raouf16/testpref");
 		
 		df.show();
-		RelationalGroupedDataset df_group = df.groupBy("NUMEN");
-		df_group.sum("semester").show();
+		df.select("semester").show();
+		df.groupBy("zip_code").count().show();
+	}
+
+	public static void main (String []args){
+
+		Generating.generateAgregatedFile();
 	}
 }
