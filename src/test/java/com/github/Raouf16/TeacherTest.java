f37ee54cf6d77f44d1156526334bc2d238b48c7a
LyndaBoulila <lyndaboulila@hotmail.fr>
2017-06-11 10:27:59 +0200

    unit tests

---
+++ src/test/java/com/github/Raouf16/TeacherTest.java
@@ -0,0 +1,20 @@
+package com.github.Raouf16;
+
+
+
+
+
+import org.junit.Test;
+
+import com.github.Raouf16.model.utils.teacher.Teacher;
+
+public class TeacherTest {
+	//we test if the constructor throws exception if the parameter of the constructor is null
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void test_the_exception() throws Exception {
+		Teacher t =new Teacher();
+		
+		
+	}
+	//if the test failed the error message is : java.lang.AssertionError: "Expected exception: java.lang.IndexOutOfBoundsException"
+}
